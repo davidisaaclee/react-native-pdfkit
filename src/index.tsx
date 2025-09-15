0f58ba4839e2807e0cbbf66b2d3e91eb55ac16ef
David Lee <david.isaac.lee@gmail.com>
2025-09-14 22:03:38 -0700

    Put specs under src/ again

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-export { default as PdfkitView } from '../specs/PdfkitViewNativeComponent';
-export * from '../specs/PdfkitViewNativeComponent';
+export { default as PdfkitView } from './specs/PdfkitViewNativeComponent';
+export * from './specs/PdfkitViewNativeComponent';
 export { PDFView } from './PDFView';
 export type { PDFViewProps, PDFViewMethods } from './PDFView';
