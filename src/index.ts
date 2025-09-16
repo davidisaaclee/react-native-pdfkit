acaee1a0eb1e25eabde892b59c011d420b717e58
David Lee <david.isaac.lee@gmail.com>
2025-09-15 17:21:15 -0700

    Stop exporting base native component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { default as PdfkitView } from './specs/PdfkitViewNativeComponent';
-export * from './specs/PdfkitViewNativeComponent';
 export { PDFView } from './PDFView';
 export type { PDFViewProps, PDFViewMethods } from './PDFView';
